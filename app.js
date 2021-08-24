d3ecc378a6081c85554b216ca2f170de3efcbba9
Fran Merlini <merlinifrancisco@gmail.com>
2021-08-23 23:06:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var contactoRouter = require('./routes/contacto');
+var productosRouter = require('./routes/productos');
+var disenosRouter = require('./routes/disenos');
+var coloresTexturasRouter = require('./routes/colores-texturas');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.use('/', indexRouter);
+app.use('/contacto', contactoRouter);
+app.use('/productos', productosRouter);
+app.use('/disenos', disenosRouter);
+app.use('/colores-texturas', coloresTexturasRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
